2e279efdc2fc91d73b0a3c0a24f135aaf50bb30c
vityaman <vityaman.dev@yandex.ru>
2024-07-17 15:54:28 +0300

    [replxx] Add history

--- src/repl.cpp
+++ src/repl.cpp
@@ -50,14 +50,24 @@ auto hint(const std::string& text, int& length, Color& color) -> Hints {
 } // namespace
 
 REPL::REPL() {
+  constexpr auto MaxHintRows = 4;
+  constexpr auto MaxHistorySize = 32;
+
   replxx.set_completion_callback(complete);
   replxx.set_hint_callback(hint);
-  replxx.set_max_hint_rows(4);
+  replxx.set_max_hint_rows(MaxHintRows);
+  replxx.set_max_history_size(MaxHistorySize);
 }
 
 auto REPL::input(const std::string& prompt) -> std::optional<std::string> {
   const auto* input = replxx.input(prompt);
-  return input != nullptr ? std::optional(input) : std::nullopt;
+  if (input == nullptr) {
+    return std::nullopt;
+  }
+
+  std::string line = input;
+  replxx.history_add(line);
+  return std::move(line);
 }
 
 auto REPL::out() -> std::ostream& {
